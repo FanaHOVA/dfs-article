3e0156420a65d83e2f1699eeeb5425cfc4ea94c6
Alessio Fanelli <fanahova@gmail.com>
2017-06-27 16:44:55 +0200

    Add insert

--- tracker.rb
+++ tracker.rb
@@ -1,4 +1,13 @@
 require './database'
 require './csv_analysis'
 
+URL = 'https://www.draftkings.com/lineup/getavailableplayerscsv?contestTypeId=28&draftGroupId=14039'.freeze
+
 Database.setup
+CsvAnalysis.scrape(URL)
+
+players = DB[:players]
+
+CsvAnalysis.format_for_database.each do |player|
+  players.insert(player)
+end
