79818be129fa86321404876b140314c8ccac367e
Alessio Fanelli <fanahova@gmail.com>
2017-06-30 12:29:39 +0200

    Isolate code and add testing script

--- notification.rb
+++ notification.rb
@@ -2,14 +2,19 @@ require 'RMagick'
 
 module Notification
   def self.mms(player)
+    generate_image(player)
+
+  end
+
+  def self.generate_image(player)
     canvas = Magick::Image.new(480, 700) {self.background_color = 'yellow'}
     gc = Magick::Draw.new
-    gc.pointsize(50)
+    gc.pointsize(15)
     gc.text(10,70, player[:name].center(11))
     gc.text(10,120, player[:team].center(11))
     gc.text(10,150, player[:salary].center(11))
 
     gc.draw(canvas)
-    canvas.write('tst.png')
+    canvas.write('./image.png')
   end
 end
