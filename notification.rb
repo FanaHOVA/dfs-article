04df970ed12258b7ed7bf86c22d25b136aae0c2f
Alessio Fanelli <fanahova@gmail.com>
2017-06-30 12:41:05 +0200

    Support array of players

--- notification.rb
+++ notification.rb
@@ -1,21 +1,25 @@
 require 'RMagick'
 
 module Notification
-  def self.mms(player)
-    info = "#{player[:name]} (#{player[:team]}) now costs #{player[:salary]}"
-    generate_image(info)
+  def self.mms(players)
+    players.map! { |player| "#{player[:name]} (#{player[:team]}) now costs #{player[:salary]}" }
+    puts players
+    generate_image(players)
 
   end
 
-  def self.generate_image(info)
-    canvas = Magick::Image.new(480, 700) {self.background_color = 'yellow'}
+  def self.generate_image(infos)
+    canvas = Magick::Image.new(480, 700) {self.background_color = 'white'}
     gc = Magick::Draw.new
     gc.pointsize(20)
     title = 'New DFS updates:'
     gc.text(10, 30, title.center(11))
-    gc.text(10, 60, info.center(11))
 
-    gc.draw(canvas)
+    infos.each do |info|
+      gc.text(10, 60, info.center(11))
+      gc.draw(canvas)
+    end
+
     canvas.write('./image.png')
   end
 end
--- tracker.rb
+++ tracker.rb
@@ -7,18 +7,19 @@ URL = 'https://www.draftkings.com/lineup/getavailableplayerscsv?contestTypeId=28
 Database.setup
 CsvAnalysis.scrape(URL)
 
-players = DB[:players]
+players_table = DB[:players]
+price_changes = []
 
 CsvAnalysis.format_for_database.each do |player|
   # Make sure we check both name and team to avoid omonims
   existing_player = DB[:players].where(name: player[:name], team: player[:team]).first
-  puts "#{existing_player}"
+
   if existing_player
-    puts "Called with #{existing_player}"
     #next unless existing_player[:salary].to_i != player[:salary].to_i
-    Notification.mms(existing_player)
+    price_changes << existing_player
   else
-    Notification.mms(player)
-    players.insert(player)
+    players_table.insert(player)
   end
 end
+
+Notification.mms(price_changes)
